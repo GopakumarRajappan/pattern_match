148aa2668cdde54976c566d859cb1ff0b17d7e51
GopakumarRajappan <gopakumar.rajappan01@gmail.com>
2023-12-11 18:50:18 +1100

    Added the logic for parenthesis check
    Logic for verifying the input parenthesis pattern is added.

--- identify_parenthesis_pattern.py
+++ identify_parenthesis_pattern.py
@@ -9,15 +9,42 @@ def check_pattern(input_pattern):
             count_closing_braces += 1
     
     if count_opening_braces != count_closing_braces:
+        print("unequal number of open and closing braces in the pattern detected")
         return False
     else:
         # logic for validation has to go here
-        return True
+
+        # define a list to store opening braces '('
+        list_of_open_braces = []
+        
+        # Iterate through the string of braces and append open brace to the list.
+        # Whenever we encounter a closing brace and the list is non-empty,
+        # remove an open brace from the end of the list. Idea is that after the 
+        # iteration is over, since we have equal number of open and closing braces,
+        # if the pattern is proper the list will be empty.
+        # eg: valid case:
+        #           pattern = (()) . logic: two append and two pop from list.
+        #     invalid case:
+        #           pattern = ))((() logic: three append one pop. non zero size for list.
+
+        for element in input_pattern:
+            if element == '(':
+                list_of_open_braces.append(element)
+
+            # even if count is same, pattern can be wrong . eg: ())(
+            # This means list shouldn't be empty while encountering a closing brace.    
+            elif element == ')' and len(list_of_open_braces) != 0:
+                list_of_open_braces.pop()
+            elif element == ')' and len(list_of_open_braces) == 0:
+                print("Pattern is invalid")
+                return False
+        if len(list_of_open_braces) == 0:
+            print("Pattern is valid")
+            return True
+        else:
+            return False
 
 if __name__ == '__main__':
     input_pattern = input("Enter the string pattern: ").strip()
     checked_result = check_pattern(input_pattern)
-    if not checked_result:
-        print("improper pattern proper")
-    else:
-        print("Proper pattern")
+    print(checked_result)
