3dffeed9057aa8938b8eb9fa8257005720e92c7a
GopakumarRajappan <gopakumar.rajappan01@gmail.com>
2023-12-15 10:41:50 +1100

    deleted commented out code

--- identify_parenthesis_pattern.py
+++ identify_parenthesis_pattern.py
@@ -1,22 +1,9 @@
 def check_pattern(input_pattern):
-    ## First check if the count of '(' and ')' are same. If not, return False. 
-    # count_opening_braces = 0
-    # count_closing_braces = 0
-    # for char in input_pattern:
-    #     if char == '(':
-    #         count_opening_braces += 1
-    #     if char == ')':
-    #         count_closing_braces += 1
-    
-    # if count_opening_braces != count_closing_braces:
-    #     print("unequal number of open and closing braces in the pattern detected")
-    #     return False
+    # Early exit if odd number of braces.
     if len(input_pattern) % 2 != 0:
         print("unequal number of open and closing braces in the pattern detected")
         return False
     else:
-        # logic for validation has to go here
-
         # define a list to store opening braces '('
         list_of_open_braces = []
         
